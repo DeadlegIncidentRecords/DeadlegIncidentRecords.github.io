d7db2d52e107303062e7a562434fe14167053d7d
HeavenOfAnimals <vaporwes@gmail.com>
2018-02-11 19:23:52 -0500

    contact

--- about/contact.php
+++ about/contact.php
@@ -1,4 +1,4 @@
-$field_name = $_POST['cf_name'];
+/*$field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 $field_subject = $_POST['cf_subject'];
@@ -18,19 +18,15 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
     <script language="javascript" type="text/javascript">
-        // Print a message
         alert('Thank you for the message! We back to you shortly.');
-        // Redirect to some page of the site. You can also specify full URL, e.g. http://template-help.com
-        // window.location = 'contact_page.html';
     </script>
 <?php
 }
 else { ?>
     <script language="javascript" type="text/javascript">
-        // Print a message
         alert("Message failed! We're sorry! Please, send an email to deadlegincidentrecords@gmail.com");
-        // Redirect to some page of the site. You can also specify full URL, e.g. http://template-help.com
-        // window.location = 'contact_page.html';
     </script>
 <?php
 }?>
+*/
+<?php echo "hello world"; ?>
